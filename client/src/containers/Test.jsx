eb2c1073939017f1fc3a5a332f3c916620554373
wukelang <lukey1321@gmail.com>
2022-08-25 23:24:08 +1200

    Refine testing page CSS and answer appearance
    Replaced buttons elements with radio inputs.
    Tried working adding JS scripts to change the CSS of an answer when
    it's selected (e.g background color) but it took too much time

--- client/src/containers/Test.jsx
+++ client/src/containers/Test.jsx
@@ -1,20 +1,44 @@
 import '../styles/Test.css';
-import questionImage from '../assets/questions/perception1.png'  // Temporary image
+
+// Function to get all images from a folder
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+const images = importAll(require.context('../assets/questions/perception1', false, /\.(png|jpe?g|svg)$/)); // Answer is c
 
 const Test = () => {
     return(
         <div className='test'>
             <div className='test__content'>
                 <div className='test__question'>
-                    <img src={questionImage} className='test__image' alt=""/>
+                    <img src={images['question.png']} className='test__image' alt=''/>
                     <p>Which of the following cubes can you make with these four pieces?</p>
                 </div>
+
                 <div className='test__answers'>
-                    <button>a</button>
-                    <button>b</button>
-                    <button>c</button>
-                    <button>d</button>
-                    <button>e</button>
+                    <div className='answer__option'>
+                        <label for="a"><img src={images['a.png']} alt=''/></label>
+                        <input type="radio" id="a" value="a" name="answer"/>
+                    </div>
+                    <div className='answer__option'>
+                        <label for="b"><img src={images['b.png']} alt=''/></label>
+                        <input type="radio" id="b" value="b" name="answer"/>
+                    </div>
+                    <div className='answer__option'>
+                        <label for="c"><img src={images['c.png']} alt=''/></label>
+                        <input type="radio" id="c" value="c" name="answer"/>
+                    </div>
+                    <div className='answer__option'>
+                        <label for="d"><img src={images['d.png']} alt=''/></label>
+                        <input type="radio" id="d" value="d" name="answer"/>
+                    </div>
+                    <div className='answer__option'>
+                        <label for="e"><img src={images['e.png']} alt=''/></label>
+                        <input type="radio" id="e" value="e" name="answer"/>
+                    </div>
+
                 </div>
             </div>
         </div>
