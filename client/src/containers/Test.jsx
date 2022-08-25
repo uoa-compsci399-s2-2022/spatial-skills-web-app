89fdf8e261b8c9f81c764cb5cbe0d56b9dcad578
wukelang <lukey1321@gmail.com>
2022-08-25 15:34:38 +1200

    Wrote basic HTML for testing page.
    Added a questions directory under client assets.
    Currently using a placeholder image with 5 answers.

--- client/src/containers/Test.jsx
+++ client/src/containers/Test.jsx
@@ -1,6 +1,23 @@
+import questionImage from '../assets/questions/perception1.png'  // Temporary image
+
 const Test = () => {
     return(
-        <div className="test">Testing page</div>
+        <div className='test'>
+            <div className='question'>
+                <img src={questionImage} className='questionImage' alt=""/>
+                <p>Which of the following cubes can you make with these four pieces?</p>
+            </div>
+            <div className='multichoiceAnswers'>
+                <button>a</button>
+                <button>b</button>
+                <button>c</button>
+                <button>d</button>
+                <button>e</button>
+            </div>
+
+        </div>
+        
+
     );
 }
 
