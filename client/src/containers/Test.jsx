e7b8097dcbf39ca3e614b3ac44e504a349c0eaee
wukelang <lukey1321@gmail.com>
2022-08-25 17:56:54 +1200

    Wrote basic CSS for testing page
    The answer div is changes in width in accordance to the question width.
    I would ideally like to look into it, but it's nothing major currently
    
    Going to further polish and add images to answer buttons.

--- client/src/containers/Test.jsx
+++ client/src/containers/Test.jsx
@@ -1,22 +1,23 @@
+import '../styles/Test.css';
 import questionImage from '../assets/questions/perception1.png'  // Temporary image
 
 const Test = () => {
     return(
         <div className='test'>
-            <div className='question'>
-                <img src={questionImage} className='questionImage' alt=""/>
-                <p>Which of the following cubes can you make with these four pieces?</p>
+            <div className='test__content'>
+                <div className='test__question'>
+                    <img src={questionImage} className='test__image' alt=""/>
+                    <p>Which of the following cubes can you make with these four pieces?</p>
+                </div>
+                <div className='test__answers'>
+                    <button>a</button>
+                    <button>b</button>
+                    <button>c</button>
+                    <button>d</button>
+                    <button>e</button>
+                </div>
             </div>
-            <div className='multichoiceAnswers'>
-                <button>a</button>
-                <button>b</button>
-                <button>c</button>
-                <button>d</button>
-                <button>e</button>
-            </div>
-
         </div>
-        
 
     );
 }
