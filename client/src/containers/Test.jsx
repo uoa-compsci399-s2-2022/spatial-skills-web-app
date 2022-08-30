b5743c57def00c86c5f62398d239caf411c097dd
wukelang <lukey1321@gmail.com>
2022-08-31 01:38:48 +1200

    Remade original layout using components
    It's likely we'll have to rewrite a lot of the code once we know what the backend is like
    and how it stores / retrieves the question data. Which is good since this code
    is really rough, its mainly just a proof of concept at this stage

--- client/src/containers/Test.jsx
+++ client/src/containers/Test.jsx
@@ -1,49 +1,105 @@
 import '../styles/Test.css';
+import React from 'react';
 
-// Function to get all images from a folder
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-const images = importAll(require.context('../assets/questions/perception1', false, /\.(png|jpe?g|svg)$/)); // Answer is c
 
-const Test = () => {
-    return(
-        <div className='test'>
-            <div className='test__content'>
-                <div className='test__question'>
-                    <img src={images['question.png']} className='test__image' alt=''/>
-                    <p>Which of the following cubes can you make with these four pieces?</p>
-                </div>
-
-                <div className='test__answers'>
-                    <div className='answer__option'>
-                        <label for="a"><img src={images['a.png']} alt=''/></label>
-                        <input type="radio" id="a" value="a" name="answer"/>
-                    </div>
-                    <div className='answer__option'>
-                        <label for="b"><img src={images['b.png']} alt=''/></label>
-                        <input type="radio" id="b" value="b" name="answer"/>
-                    </div>
-                    <div className='answer__option'>
-                        <label for="c"><img src={images['c.png']} alt=''/></label>
-                        <input type="radio" id="c" value="c" name="answer"/>
-                    </div>
-                    <div className='answer__option'>
-                        <label for="d"><img src={images['d.png']} alt=''/></label>
-                        <input type="radio" id="d" value="d" name="answer"/>
-                    </div>
-                    <div className='answer__option'>
-                        <label for="e"><img src={images['e.png']} alt=''/></label>
-                        <input type="radio" id="e" value="e" name="answer"/>
-                    </div>
-
-                </div>
+class Test extends React.Component {
+    render() {
+        return (
+            <div className='test'>
+                <TestContent question={images}/>
+                <NextButton />
             </div>
-        </div>
-
-    );
+        )
+    }
 }
 
+
+class NextButton extends React.Component {
+    render() {
+        return (
+            <button>Next</button>
+        )
+    }
+}
+
+
+class TestContent extends React.Component {
+    render() {
+        return (
+            <div className='test__content'>
+                <Question question={this.props.question}/>
+                <Answer question={this.props.question}/>
+            </div>
+        )
+    }
+}
+
+class Question extends React.Component {
+    renderQuestionImage() {
+        const image = this.props.question[this.props.question.length - 1]
+        return image;
+    }
+
+    render() {
+        return (
+            <div className='test__question'>
+                <img src={this.renderQuestionImage()} className='test__image' alt=''/>
+                <p>Which of the following cubes can you make with these four pieces?</p>
+            </div>
+        )
+    }
+}
+
+class Answer extends React.Component {
+    renderMultiChoiceAnswers() {
+        const answers = [];
+        const labels = ["a", "b", "c", "d", "e"];
+        for (var i = 0; i < this.props.question.length - 1; i++) {
+            answers.push(
+                <MultichoiceAnswer 
+                    image={this.props.question[i]}
+                    label={labels[i]}
+                />
+            );
+        }
+        return answers;
+    }
+
+    render() {  // Currently only allows multichoice answers. We should also make an entry type answer
+        return (
+            <div className='test__answers'>
+                {this.renderMultiChoiceAnswers()}
+            </div>
+        )
+    }
+}
+
+function MultichoiceAnswer(props) {
+    return (
+        <div className='answer__option'>
+            <label for={props.label}><img src={props.image} alt=''/></label>
+            <input type="radio" id={props.label} value={props.label} name="answer"/>
+        </div>
+    )
+}
+
+
 export default Test;
+
+
+// ----------- Local Question Importing
+
+
+// Function to get all images from a folder as an array.
+// Array order corresponds to folder order (question is last).
+function importAll(r) {
+    let images = [];
+    r.keys().map((item, index) => { images.push(r(item)); });
+    return images;
+
+    ////Use these line if you want to access each image using the file name.
+    //let images = {};
+    //r.keys().map((item, index) => { images[item.replace('./','')] = r(item); });
+}
+const images = importAll(require.context('../assets/questions/perception1', false, /\.(png|jpe?g|svg)$/));  // ans = c
+const images2 = importAll(require.context('../assets/questions/perception4', false, /\.(png|jpe?g|svg)$/));
