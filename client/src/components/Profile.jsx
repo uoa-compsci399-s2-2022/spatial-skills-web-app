1a1574737d2a689299168503974853e31486b6e0
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-09-02 14:43:56 +1200

    Add profile component
    App will use public Google account information when a user is logged in (name, email and picture). Alternatively users can bypass Google login by entering a name.

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,37 @@
+
+import '../styles/Profile.css';
+import { FaUserCircle } from 'react-icons/fa';
+import { useState } from 'react';
+import { googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+
+const Profile = (props) => {
+    const [expanded, setExpanded] = useState(false);
+    const { name, email, picture, setUserData } = props;
+    const navigate = useNavigate();
+    return(
+        <div className='profile' >
+            
+            <button onClick={() => setExpanded(expanded ? false : true)}>
+            {
+                picture === null ?
+                <FaUserCircle size="2rem" color="var(--contrast-color)" /> :
+                <img src={picture} alt="" className='profile__picture' />
+            }
+            </button>
+
+            {   
+                expanded && name !== null ?
+                <div className='profile__text-container'>
+                    <p>Signed in as<br /><b>{name}</b><br />{email}</p>
+                    <button className='profile__logout' onClick={() => {googleLogout(); setUserData({name: null, email: null, picture: null}); navigate('/')} }>Sign out</button>
+                </div> :
+                null
+            }
+            
+            
+        </div>
+    );
+}
+
+export default Profile;
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -2,37 +2,63 @@ import '../styles/Home.css';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin} from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 import logo from '../assets/logo.png';
+import Profile from '../components/Profile';
 
 const Home = () => {
     const navigate = useNavigate();
     const nameRef = useRef(null);
     const [error, setError] = useState("");
-    const [userName, setUserName] = useState(null);
+    const [userData, setUserData] = useState({
+        name: null,
+        email: null,
+        picture: null
+    });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();    // this stops the page from reloading by default
+    const handleSubmit = () => {
         setError("");
         const name = nameRef.current.value;
         if (name.length === 0) {
             setError("Invalid name!");
         } else {
-            setUserName(name);
-            console.log(name)    // this is the input name
+            setUserData({
+                name: name,
+                email: null,
+                picture: null
+            })
             navigate('/test');     // go to test page 
         }        
     }
 
+    const handleGoogleLogin = (credentials) => {
+        setError("");
+        const payload = jwt_decode(credentials);
+        setUserData({
+            name: payload.given_name + " " + payload.family_name,
+            email: payload.email,
+            picture: payload.picture
+        })
+        navigate('/test');
+    }
+
     return(
         <div className='home'>
+            {/* the Profile component should be moved into the navbar */}
+            <Profile 
+                name={userData.name} 
+                email={userData.email} 
+                picture={userData.picture} 
+                setUserData={setUserData}
+            />
             <img src={logo} className='home__logo' alt="" />
             <div className='home__content'>
                 <GoogleLogin 
-                    onSuccess={(credentialResponse) => console.log(credentialResponse)}
+                    onSuccess={(credentialResponse) => handleGoogleLogin(credentialResponse.credential)}
                     onError={() => setError("Unable to authenticate Google account!")}
                     width='240'
                 />
-                <form onSubmit={(e) => handleSubmit(e)} className='home__form'>
+                <form onSubmit={(e) => { e.preventDefault(); handleSubmit() }} className='home__form'>
                     <input 
                         type="text" 
                         placeholder="Name" 
