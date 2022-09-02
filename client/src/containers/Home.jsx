e9f80c49a4b2367f254fc6dbce282574530692c4
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-09-02 12:05:28 +1200

    Add error handling for invalid names

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,11 +1,30 @@
 import '../styles/Home.css';
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 import { GoogleLogin} from '@react-oauth/google';
+import { useState } from 'react';
 
 const Home = () => {
     const navigate = useNavigate();
+    const nameRef = useRef(null);
+    const [displayError, setDisplayError] = useState(false);
+    const [userName, setUserName] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();    // this stops the page from reloading by default
+        setDisplayError(false);
+        const name = nameRef.current.value;
+        if (name.length === 0) {
+            setDisplayError(true);
+        } else {
+            setUserName(name);
+            console.log(name)    // this is the input name
+            navigate('/test');     // go to test page 
+        }        
+    }
+
     return(
         <div className='home'>
             <img src={logo} className='home__logo' alt="" />
@@ -15,10 +34,25 @@ const Home = () => {
                     onError={() => console.log("Failed!")}
                     width='240'
                 />
-                <form onSubmit={(e) => {e.preventDefault(); navigate('/test')}} className='home__form'>
-                    <input type="text" placeholder="Name" className='home__input home__input--text' />
-                    <button className='home__input home__input--button'>Enter</button>
+                <form onSubmit={(e) => handleSubmit(e)} className='home__form'>
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        className='home__input home__input--text' 
+                        ref={nameRef} 
+                    />
+                    {
+                        displayError ?
+                        <p className='home__error'>Invalid Name!</p> :
+                        null
+                    }
+                    <button 
+                        className='home__input home__input--button'
+                    >
+                        Enter
+                    </button>
                 </form>
+                
             </div>
         </div>
     );
