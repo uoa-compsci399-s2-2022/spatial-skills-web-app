1d53746f66cc3caebdb44f2a0d9230e122131faf
Vladd242 <110016721+Vladd242@users.noreply.github.com>
2022-09-06 17:25:41 +1200

    Added Popup Destructuring

--- client/src/components/Popup.jsx
+++ client/src/components/Popup.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import '../styles/Navbar.css'
 
 function Popup(props) {
-    const { trigger } = props;
+    const { trigger, children, setTrigger } = props;
 
     return(trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn" onClick={() => props.setTrigger(false)}>
+                <button className="close-btn" onClick={() => setTrigger(false)}>
                 <b>X</b></button>
-                {props.children}
+                {children}
             </div> 
         </div>
     ) : "";
