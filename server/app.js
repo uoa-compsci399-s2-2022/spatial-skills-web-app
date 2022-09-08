fcb626bb6c76317f160b3b9ad799ec1e05d4048b
wukelang <lukey1321@gmail.com>
2022-09-08 14:12:50 +1200

    Fix CORS issue, fetch can access local endpoints

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 import connectDB from './db/db.js';
 import questionRouter from './routes/question-routes.js';
+import cors from "cors";  // https://www.npmjs.com/package/cors
 
 
 const app = express();
@@ -11,11 +12,13 @@ const port = process.env.PORT || 3001;
 // Connect to database
 connectDB();
 
+app.use(cors());    // Allows fetch api to access localhost endpoints
 //parse json requests
 app.use(express.json());
 
 //question api's
 app.use('/api/question',questionRouter);
+
 
 app.get('/', (req, res) => res.send("Hello world!"));
 
