d71f2cb9bc58511ebed338ae6b9e9639d6ff7749
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-09-15 18:24:52 +1200

    Update title, brand and link bank to editor

--- client/src/containers/Bank.jsx
+++ client/src/containers/Bank.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/Bank.css";
 import Questions from "./questions.json";
 import { Link } from "react-router-dom";
-import { FaPlus } from "react-icons/fa"
+import { FaPlus } from "react-icons/fa";
 
 const Bank = () => {
   const [optionValue, setOptionValue] = useState("All");
@@ -10,8 +10,8 @@ const Bank = () => {
   const handleSelect = (e) => {
     setOptionValue(e.target.value);
   };
-  function getInput(val){
-    setData(val.target.value)
+  function getInput(val) {
+    setData(val.target.value);
   }
 
   return (
@@ -23,36 +23,39 @@ const Bank = () => {
           id="category"
           onChange={handleSelect}
           className="bank-input-select"
-          >
+        >
           <option value="All">All</option>
           <option value="Spatial Memory">Spatial Memory</option>
-          <option value="Visuospatial Perception">Visuospatial Perception</option>
+          <option value="Visuospatial Perception">
+            Visuospatial Perception
+          </option>
           <option value="Mental Rotation">Mental Rotation</option>
           <option value="Spatial Visualisation">Spatial Visualisation</option>
-          </select>
-          <input type="text" className="bank-input" onChange={getInput}></input>
+        </select>
+        <input type="text" className="bank-input" onChange={getInput}></input>
       </div>
 
       <div className="bank-right-panel">
         <h1>Question Bank</h1>
         <hr />
-        {
-          Questions &&
+        {Questions &&
           Questions.map((question) => {
-            if((question.category === optionValue || optionValue === "All") && (question.title.toLowerCase().includes(data.toLowerCase()))){
+            if (
+              (question.category === optionValue || optionValue === "All") &&
+              question.title.toLowerCase().includes(data.toLowerCase())
+            ) {
               return (
-                <div className = "bank-test-elements">
-                  <Link to={{
-                    pathname: "/dashboard/bank", // this would be assigned to the path of the editor
-                    search: question._id // passes the id of the question being selected.
-                  }}>
-                  <button onClick={() => console.log(`EDIT "${question.title}"`)}>
-                  <img
-                  className="bank-test-button"
-                  alt="QuestionImage"
-                  src={question.image}
-                  ></img>
-                  </button>
+                <div className="bank-test-elements">
+                  <Link to={`/dashboard/bank/editor/${question._id}`}>
+                    <button
+                      onClick={() => console.log(`EDIT "${question.title}"`)}
+                    >
+                      <img
+                        className="bank-test-button"
+                        alt="QuestionImage"
+                        src={question.image}
+                      ></img>
+                    </button>
                   </Link>
                   <div className="bank-test-text">
                     <h4>{question.title}</h4>
@@ -61,20 +64,20 @@ const Bank = () => {
                       className="bank-remove-button"
                       title="Remove"
                       onClick={() => console.log(`REMOVE "${question.title}"`)}
-                    >X
+                    >
+                      X
                     </button>
                   </div>
                 </div>
               );
             }
-          })
-        }
+          })}
 
-        <div className = "bank-test-elements">
+        <div className="bank-test-elements">
           <Link to="/dashboard/bank">
-          <div className="bank-test-button">
-            <FaPlus size="5rem" />
-          </div>
+            <div className="bank-test-button">
+              <FaPlus size="5rem" />
+            </div>
           </Link>
           <div className="bank-test-text">
             <h4>Create Question</h4>
