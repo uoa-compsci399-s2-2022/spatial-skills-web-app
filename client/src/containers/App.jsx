2e679da3d5cae64e55a166d7e67177b8670a584a
JackHH7297 <jackhuang7297@gmail.com>
2022-09-28 22:49:28 +1300

    resolve conflict attempt

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "../styles/App.css";
 import Home from "./Home";
 import Test from "./Test";
 import TestResult from "./TestResult";
+import AdminLogin from "./AdminLogin";
+import Dashboard from "./Dashboard";
+import Stats from "./Stats";
+import Bank from "./Bank";
+import Editor from "./Editor";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Splash from "../components/Splash";
-
-import PatternGame from "../components/PatternGame/PatternGame";
 
 const GOOGLE_CLIENT_ID =
   "126878629767-elluidkp4g3iost84sgh5spapdq30su7.apps.googleusercontent.com";
@@ -19,6 +22,7 @@ function App() {
     email: null,
     picture: null,
   });
+
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <BrowserRouter>
@@ -28,12 +32,40 @@ function App() {
             path="/"
             element={<Home userData={userData} setUserData={setUserData} />}
           />
+          <Route
+            path="/adminLogin"
+            element={
+              <AdminLogin
+                userData={userData}
+                setUserData={setUserData}
+              />
+            }
+          />
           <Route path="/test" element={<Test userData={userData} />} />
           <Route path="/results/:tId/:sId" element={<TestResult />} />
-
+          <Route
+            path="*"
+            element={
+              sessionStorage.getItem("redirectAdmin") === "true" ? (
+                <Navigate replace to="/adminLogin" />
+              ) : (
+                <Navigate replace to="/" />
+              )
+            }
+          <Route path="test" element={<Test />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard/test/:testId/stats" element={<Stats />} />
+          <Route path="dashboard/test/:testId/question" element={<Bank />} />
+          <Route
+            path="dashboard/test/:testId/question/:questionId"
+            element={<Editor userData={userData} isTest={false} />}
+          />
+          <Route
+            path="dashboard/test/:testId"
+            element={<Editor userData={userData} isTest={true} />}
+          />
         </Routes>
         <Footer />
-        <Splash />
       </BrowserRouter>
     </GoogleOAuthProvider>
   );
