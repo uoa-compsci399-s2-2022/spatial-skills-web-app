723e312fb1158c31396b9529cc1a946ee0d9e1eb
JackHH7297 <jackhuang7297@gmail.com>
2022-10-20 20:30:19 +1300

    delete test routes and integrate partial mark calculation

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -74,48 +74,7 @@ function App() {
             path="dashboard/test/:code"
             element={<Editor userData={userData} />}
           />          
-          <Route
-            path="pattern"
-            element={
-            <PatternGame 
-            corsi={true}
-            gameDim={5}       // width and height of grid
-            order={true}        // pattern order/no-order
-            reverse={true}      // replicate the pattern in reverse order, order must be true
-            maxHealth={5}
-            timerState={true}       // set timer on/off
-            timeAllowed={60}          // total time if timer on
-            patternFlashTime={0.5}      // time to flash each pattern block
-            randomLevelOrder={true}      // each level is randomized
-            randomSeed={"just a seed"}
-            description={
-              "Click on the pattern shown at the start of the game, \
-              You will lose a life for each mismatch. \
-              Progress as far as you can! \
-              Click start to begin."}
 
-            changeState={changeState}
-            />}
-          />
-
-          <Route
-            path="matching"
-            element={
-            <MatchingGame
-            pairs={8}                // number of pairs to match
-            gameStartDelay={1}       // time to reveal all cards beginning of game
-            selectionDelay={1}       // reveal time after a mismatch between 2 cards
-            maxHealth={5}
-            randomSeed={"Hi"}         
-            timerState={true}       // set timer on/off
-            timeAllowed={2}         // total time if timer on
-            description={
-            "Match those cards in pairs before time runs out! \
-            You will lose a life for each mismatch. \
-            Click start to begin."
-            }
-            />}
-          />
         </Routes>
         <Footer />
       </BrowserRouter>
